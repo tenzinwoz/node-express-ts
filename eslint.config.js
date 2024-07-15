233fa082549c0d1a04305a2689d33c4d424a3122
Tenzin Woeser <tenzin.woeser@swish.ca>
2024-07-14 22:15:39 -0400

    feat: Setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,20 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended
-);
+export default tseslint.config({
+  languageOptions: {
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  files: ["**/*.ts"],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+  ],
+  rules: {
+    "no-console": "error",
+    quotes: ["error", "single", { allowTemplateLiterals: true }],
+  },
+});
